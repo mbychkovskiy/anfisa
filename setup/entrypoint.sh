21b22994e7dcd88246a62d4083e48c12bc4b8306
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-10-06 13:04:27 +0300

    Update entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -4,9 +4,9 @@ if [ ${COUNT_FILES} -le 2 ] ; then
     mkdir -p /anfisa/a-setup/{data,logs,vault,export/work,ui}
 fi
 
-if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
-then
-    sed "s#anfisa-test-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#anfisa-test-router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa-test-mongo#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
-fi
+#if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
+#then
+#    sed "s#anfisa-test-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#anfisa-test-router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa-test-mongo#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
+#fi
 
 exec "$@"
